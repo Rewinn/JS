ce87f861cfa2ae3fe87dfd717db432b8ffb0d82d
rewinn <rachel.winn@me.com>
2015-03-13 19:30:15 -0500

    @mrcwinn Form and Slider

---
+++ JS Slider/js/slider.js
@@ -0,0 +1,34 @@
+var elem = document.querySelector('input[type="range"]');
+
+var rangeValue = function() {
+    var newValue     = elem.value;
+    var target       = document.querySelector('.value');
+    target.innerHTML = newValue;
+}
+
+elem.addEventListener('input', rangeValue);
+
+
+
+/*
+var elem = document.querySelector('input[type="range"]');
+
+var rangeValue = function() {
+    var newValue     = elem.value;
+    var target       = document.querySelector('.value');
+    target.innerHTML = newValue;
+}
+
+elem.addEventListener('input', rangeValue);
+
+
+var elem = document.querySelector('input [type="range"]');
+
+var rangeValue = function() {
+    var newValue     = elem.value;
+    var target       = document.querySelector('.value');
+    target.innerHTML = newValue;
+}
+
+elem.addEventListener('input', rangeValue);
+*/
