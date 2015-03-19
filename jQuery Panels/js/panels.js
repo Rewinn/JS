462a3a8c45470be803100d74212b0064e696cce7
rewinn <rachel.winn@me.com>
2015-03-19 18:55:46 -0500

    @mrcwinn

---
+++ jQuery Panels/js/panels.js
@@ -0,0 +1,19 @@
+// Green Button
+$('#expand-green').on('click', function() {
+	$('.text').toggleClass('open-green');
+	$(this).toggleClass('close-green');
+});
+
+//Red Button
+$('#expand-red').on('click', function() {
+	$('.text').toggleClass('open-red');
+	$(this).toggleClass('close-red');
+});
+
+//Blue Button
+$('#expand-sky').on('click', function() {
+	$('.text').toggleClass('open-sky');
+	$(this).toggleClass('close-sky');
+});
+
+
